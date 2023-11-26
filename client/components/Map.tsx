d35a695909216406f8fcd514297876feab9f8b88
Katie Riches <katiegee2011@gmail.com>
2023-11-27 12:25:01 +1300

    Remove second pop up

--- client/components/Map.tsx
+++ client/components/Map.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState, SetStateAction } from 'react'
 import mapboxgl from 'mapbox-gl'
 import './App.css'
 import MapClickHandler from './AddLocation'
 import RatingForm from './RatingForm'
+import Popup from './ShopForm' // Import your PopupBase component
 
 const REACT_APP_MAPBOX_ACCESS_TOKEN =
   'pk.eyJ1Ijoia2F0aWVnZWUiLCJhIjoiY2xvcWNxbDhrMGhmbjJrbWM1ZWFtZHIzcCJ9.BNmn1eIG0EBZkXUr4SuxDQ'
@@ -12,6 +12,8 @@ mapboxgl.accessToken = REACT_APP_MAPBOX_ACCESS_TOKEN
 
 const Map = () => {
   const mapContainerRef = useRef(null)
+  const [popupLngLat, setPopupLngLat] = useState(null)
+  const [content, setContent] = useState<string | null>(null)
 
   useEffect(() => {
     // Check if the Geolocation API is supported by the browser
@@ -30,20 +32,14 @@ const Map = () => {
             center: [userLng, userLat],
             zoom: 12.5,
           })
+
           MapClickHandler(map)
+
           // add navigation control
           map.addControl(new mapboxgl.NavigationControl(), 'bottom-right')
 
-          //add a marker at users location
+          // add a marker at the user's location
           new mapboxgl.Marker().setLngLat([userLng, userLat]).addTo(map)
-
-          // add click event listner to the map
-          map.on('click', (e) => {
-            const { lng, lat } = e.lngLat
-
-            //Creates the marker
-            new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map)
-          })
 
           // clean up on unmount
           return () => map.remove()
@@ -57,7 +53,11 @@ const Map = () => {
     }
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
-  return <div className="map-container" ref={mapContainerRef} />
+  return (
+    <>
+      <div className="map-container" ref={mapContainerRef} />
+    </>
+  )
 }
 
 export default Map
